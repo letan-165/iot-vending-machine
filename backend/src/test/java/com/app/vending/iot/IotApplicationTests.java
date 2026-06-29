66096a6f9ad0f8b7adbee4459e01ddf686ea9749
letan-165 <2251160057@ut.edu.vn>
2026-06-29 23:41:36 +0700

    chore: init be

---
+++ backend/src/test/java/com/app/vending/iot/IotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.vending.iot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
