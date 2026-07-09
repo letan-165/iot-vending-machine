90729f6facabc1bd272692f7f19989c3019317a8
letan-165 <2251160057@ut.edu.vn>
2026-07-09 16:02:59 +0700

    refactor: BaseAddress

--- frontend/guest/vending/vending/service/ApiClient.cs
+++ frontend/guest/vending/vending/service/ApiClient.cs
@@ -10,7 +10,8 @@ namespace vending.network
     {
         private static readonly HttpClient client = new HttpClient()
         {
-            BaseAddress = new Uri("http://localhost:8080/api/")
+//            BaseAddress = new Uri("http://localhost:8080/api/")
+            BaseAddress = new Uri("https://p01--iot-vending-machine--9w4j25rlpvhz.code.run/api/")
         };
 
         public static HttpClient Client => client;
